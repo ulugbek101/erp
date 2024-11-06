3ff3674b9d4dac95dd0156bb15370b0f91b1ab51
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2024-11-07 00:48:18 +0500

    Build groups page UI

--- app_main/urls.py
+++ app_main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.redirect_to_home)
+    path('', views.redirect_to_home),
+    path('teacher-groups/', views.teacher_groups, name='teacher-groups'),
 ]
--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 
 def redirect_to_home(request):
     return redirect("account")
+
+
+def teacher_groups(request):
+    context = {
+        'groups': True,
+    }
+    return render(request, 'app_main/teacher-groups.html', context)
